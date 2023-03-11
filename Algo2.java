ac8742bc7c30b6514f04e59b87677d282a7e3077
yalathiya <yashlathiya59ppsv@gmail.com>
2023-03-11 12:15:41 +0530

    having issue in solution

--- Algo2.java
+++ Algo2.java
@@ -7,26 +7,49 @@ class Algo2{
         Scanner sc = new Scanner(System.in);
 
         int t = sc.nextInt();
-
-        while(t>0){
+        for(int i=0; i<t; i++){
 
             int houses = sc.nextInt();
 
-            while(houses > 0){
-
+            double totalHours = 0;
+            double accentQty = 0;
+            double regularQty = 0;
+            
+            for(int j=0; j<houses; j++){
                 int victorianStyleHalls = sc.nextInt();
                 int standerdRooms = sc.nextInt();
                 int roofBedRooms = sc.nextInt();
-
-                double totalHours = 0;
-                double accentQty = 0;
-                double regularQty = 0;
+                int kitchenRooms = sc.nextInt();
 
                 //approach to solve question
-        
-                houses--;
+
+                // for victorian style halls
+                accentQty += ((victorianStyleHalls*6)/3)*1.5;
+                totalHours += ((victorianStyleHalls*6)/3)*2.5;
+                regularQty += ((victorianStyleHalls*6*2)/3)*2.25;
+                totalHours += ((victorianStyleHalls*6*2)/3)*3.25;
+
+                // for standard rooms
+                accentQty += ((standerdRooms*4)/3)*1.5;
+                totalHours += ((standerdRooms*4)/3)*2.5;
+                regularQty += ((standerdRooms*4*2)/3)*2.25;
+                totalHours += ((standerdRooms*4*2)/3)*3.25;
+
+                //for roof bed rooms
+                accentQty += ((roofBedRooms*3)/3)*1.5;
+                totalHours += ((roofBedRooms*3)/3)*2.5;
+                regularQty += ((roofBedRooms*3*2)/3)*2.25;
+                totalHours += ((roofBedRooms*3*2)/3)*3.25;
+
+                // for kitchen rooms
+                accentQty += ((kitchenRooms*4)/3)*1.5;
+                totalHours += ((kitchenRooms*4)/3)*2.5;
+                regularQty += ((kitchenRooms*4*2)/3)*2.25;
+                totalHours += ((kitchenRooms*4*2)/3)*3.25;
+                
             }
-            t--;
+            System.out.println(totalHours+" "+accentQty+" "+regularQty);
         }
+
     }
 }
