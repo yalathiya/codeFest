5f9787b81cd4e211c6aa99e4104b55e73928b259
yalathiya <yashlathiya59ppsv@gmail.com>
2023-03-11 13:38:57 +0530

    clear

--- Algo2.java
+++ Algo2.java
@@ -6,49 +6,33 @@ class Algo2{
 
         Scanner sc = new Scanner(System.in);
 
+
         int t = sc.nextInt();
         for(int i=0; i<t; i++){
 
             int houses = sc.nextInt();
 
-            double totalHours = 0;
-            double accentQty = 0;
-            double regularQty = 0;
-            
+            double th = 0;
+            double aq = 0;
+            double rq = 0;
+            double walls = 0;
+
             for(int j=0; j<houses; j++){
                 int victorianStyleHalls = sc.nextInt();
                 int standerdRooms = sc.nextInt();
                 int roofBedRooms = sc.nextInt();
                 int kitchenRooms = sc.nextInt();
 
-                //approach to solve question
-
-                // for victorian style halls
-                accentQty += ((victorianStyleHalls*6)/3)*1.5;
-                totalHours += ((victorianStyleHalls*6)/3)*2.5;
-                regularQty += ((victorianStyleHalls*6*2)/3)*2.25;
-                totalHours += ((victorianStyleHalls*6*2)/3)*3.25;
-
-                // for standard rooms
-                accentQty += ((standerdRooms*4)/3)*1.5;
-                totalHours += ((standerdRooms*4)/3)*2.5;
-                regularQty += ((standerdRooms*4*2)/3)*2.25;
-                totalHours += ((standerdRooms*4*2)/3)*3.25;
-
-                //for roof bed rooms
-                accentQty += ((roofBedRooms*3)/3)*1.5;
-                totalHours += ((roofBedRooms*3)/3)*2.5;
-                regularQty += ((roofBedRooms*3*2)/3)*2.25;
-                totalHours += ((roofBedRooms*3*2)/3)*3.25;
-
-                // for kitchen rooms
-                accentQty += ((kitchenRooms*4)/3)*1.5;
-                totalHours += ((kitchenRooms*4)/3)*2.5;
-                regularQty += ((kitchenRooms*4*2)/3)*2.25;
-                totalHours += ((kitchenRooms*4*2)/3)*3.25;
+                walls += (victorianStyleHalls*6)+(standerdRooms*4)+(roofBedRooms*4)+(4);
+                
+                
                 
             }
-            System.out.println(totalHours+" "+accentQty+" "+regularQty);
+            aq = aq +walls*(1.0/3.0)*1.5;
+                rq = rq +walls*(2.0/3.0)*2.25;
+                th = th+(walls*(1.0/3.0)*2.5) + (walls*(2.0/3.0)*3.25);
+            System.out.println("Number of total walls"+ walls);
+            System.out.println(th+" "+aq+" "+rq);
         }
 
     }
