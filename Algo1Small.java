bdc70c45bf98bc4a5a53276cf4543ef2ef53d18a
yalathiya <yashlathiya59ppsv@gmail.com>
2023-03-11 10:57:09 +0530

    Commenting of approach

--- Algo1Small.java
+++ Algo1Small.java
@@ -7,29 +7,34 @@ class Algo1Small{
 
         int arr[][] = new int[t][3];
 
+        //input values
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<3; j++){
                 arr[i][j] = sc.nextInt();
             }
         }
             
+        //Sort array  
         Arrays.sort(arr, new Comparator<int[]>() {
         @Override
         public int compare(final int[] val1, final int[] val2) {
-            int cmp =  Integer.compare(val1[0], val2[0]);
+            int cmp =  Integer.compare(val1[0], val2[0]); //Sort array according to first column(text) [accending order]
             if(cmp == 0){
-                return Integer.compare(val2[1], val1[1]);
+                return Integer.compare(val2[1], val1[1]); //if column value is same then sort array according to second column(images) [decending order]
             }
             return cmp;
         }
         });
 
+        //print array
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<3; j++){
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
-        
+
+        sc.close();
+
     }
 }
