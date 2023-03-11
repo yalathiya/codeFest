726beeee4ef53645a7b9b5c086505465ac03e225
yalathiya <yashlathiya59ppsv@gmail.com>
2023-03-11 11:45:57 +0530

    framework of 2nd question

---
+++ Algo2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Algo2{
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t>0){
+
+            int houses = sc.nextInt();
+
+            while(houses > 0){
+
+                int victorianStyleHalls = sc.nextInt();
+                int standerdRooms = sc.nextInt();
+                int roofBedRooms = sc.nextInt();
+
+                double totalHours = 0;
+                double accentQty = 0;
+                double regularQty = 0;
+
+                //approach to solve question
+        
+                houses--;
+            }
+            t--;
+        }
+    }
+}
